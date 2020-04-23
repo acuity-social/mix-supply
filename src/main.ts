a3321aaeeb37fff7c8463bf272bc83319560012f
Jonathan Brown <jbrown@mix-blockchain.org>
2020-04-23 07:08:06 +0700

    Remove semicolons.

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,55 @@
-const Api = require('@parity/api');
-const BigNumber = require('bignumber.js');
-const express = require('express');
-const app = express();
+const Api = require('@parity/api')
+const BigNumber = require('bignumber.js')
+const express = require('express')
+const app = express()
 
-const provider = new Api.Provider.Http('http://127.0.0.1:8645');
-const api = new Api(provider);
+const provider = new Api.Provider.Http('http://127.0.0.1:8645')
+const api = new Api(provider)
 
-const abi = require('./mix_revenue.abi.json');
-const address = '0x97c7f4f8f0bbf384578a9f5754ae73f37ff49ec2';
-const contract = api.newContract(abi, address);
+const abi = require('./mix_revenue.abi.json')
+const address = '0x97c7f4f8f0bbf384578a9f5754ae73f37ff49ec2'
+const contract = api.newContract(abi, address)
 
 app.get('/', async function (req: any, res: any) {
 
   let blockNumber = await api.eth.blockNumber()
   let released = await contract.instance.getReleased.call({}, blockNumber)
   let accounts = await api.parity.listAccounts(1000000000, null, blockNumber)
-  let total = new BigNumber(api.util.toWei(-55000000, 'ether').plus(released));
-  let promises = [];
+  let total = new BigNumber(api.util.toWei(-55000000, 'ether').plus(released))
+  let promises = []
 
   for (let account of accounts) {
     promises.push(api.eth.getBalance(account, blockNumber)
       .then((balance: number) => {
-        total = total.plus(balance);
-      }));
+        total = total.plus(balance)
+      }))
   }
 
   Promise.all(promises).then(() => {
-    res.json({blockNumber: blockNumber, circulatingSupply: api.util.fromWei(total, 'ether').toString()});
-  });
-});
+    res.json({blockNumber: blockNumber, circulatingSupply: api.util.fromWei(total, 'ether').toString()})
+  })
+})
 
 app.get('/circulatingSupply', async function (req: any, res: any) {
 
   let blockNumber = await api.eth.blockNumber()
   let released = await contract.instance.getReleased.call({}, blockNumber)
   let accounts = await api.parity.listAccounts(1000000000, null, blockNumber)
-  let total = new BigNumber(api.util.toWei(-55000000, 'ether').plus(released));
-  let promises = [];
+  let total = new BigNumber(api.util.toWei(-55000000, 'ether').plus(released))
+  let promises = []
 
   for (let account of accounts) {
     promises.push(api.eth.getBalance(account, blockNumber)
       .then((balance: number) => {
-        total = total.plus(balance);
-      }));
+        total = total.plus(balance)
+      }))
   }
 
   Promise.all(promises).then(() => {
-    res.send(api.util.fromWei(total, 'ether').toString());
-  });
-});
+    res.send(api.util.fromWei(total, 'ether').toString())
+  })
+})
 
 app.listen(4000, function () {
-  console.log('MIX Supply listening on port 4000');
-});
+  console.log('MIX Supply listening on port 4000')
+})
